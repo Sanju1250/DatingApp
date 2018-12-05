f8e6e1334446676e1c1d6fa7b60901beef4170d7
satish kumar <satishkotichukkala@outlook.com>
2018-12-05 18:12:33 +0530

    end of section 9

---
+++ DatingApp-SPA/src/app/member/member-list/member-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { User } from '../../_model/User';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css']
+})
+export class MemberListComponent implements OnInit {
+  users: User[];
+
+  constructor(private userService: UserService, private alertyfy: AlertifyService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.users = data['users'];
+    });
+  }
+
+// loadUsers() {
+//  return this.userService.getUsers().subscribe((users: User[]) => {
+//    this.users = users;
+//  }, error => {
+//   this.alertyfy.error(error);
+//  }
+// );
+// }
+
+
+}
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,9 +1,12 @@
 import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './member/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './member/member-detail/member-detail.component';
+import { MemberdetailResolver } from './_resolvers/members-detail.resolver';
+import { MemberListResolver } from './_resolvers/members-List.resolver';
 export const appRoutes: Routes = [
 {path: '', component: HomeComponent},
 {
@@ -12,11 +15,13 @@ export const appRoutes: Routes = [
     canActivate: [AuthGuard],
     children: [
         {path: 'lists', component: ListsComponent},
-        { path: 'members', component: MemberListComponent},
+        { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+        { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberdetailResolver}},
         { path: 'messages', component: MessagesComponent}
     ]
 
 },
 
 {path: '**', redirectTo: '', pathMatch: 'full'}
+
 ];
